a4bc1e790af4daa647762ceb80e25f7ecca671f5
zoudaifa <daleyzou@gmail.com>
2020-09-03 09:13:02 +0800

    add Deferredresult
    Co-Authored-By: DaleyZou's World <daleyzou@163.com>
    Signed-off-by: zoudaifa <daleyzou@gmail.com>

---
+++ src/main/java/com/daleyzou/demo/list/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.daleyzou.demo.list;
+
+import java.util.ArrayList;
+
+/**
+ * ArrayListDemo
+ * @description TODO
+ * @author daleyozu
+ * @date 2020年08月22日 12:02
+ * @version 1.3.1
+ */
+public class ArrayListDemo {
+
+    public static void main(String[] args){
+        ArrayList<Integer> demo = new ArrayList<>();
+    }
+}
---
+++ src/main/java/com/daleyzou/demo/lru/LruDemo.java
@@ -0,0 +1,12 @@
+package com.daleyzou.demo.lru;
+
+/**
+ * LRUDemo
+ * @description TODO
+ * @author daleyzou
+ * @date 2020年09月03日 9:11
+ * @version 1.3.1
+ */
+public class LruDemo {
+
+}
